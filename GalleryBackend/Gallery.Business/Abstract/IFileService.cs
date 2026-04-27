1ed63f1d17eb483449ff3b7160ce43857fce437b
Zeyitcan DAŞDEMİR <zytcn01@outlook.com>
2026-04-27 23:28:20 +0300

    Implement file service

---
+++ GalleryBackend/Gallery.Business/Abstract/IFileService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Gallery.Business.Abstract
+{
+    public interface IFileService
+    {
+        Task<List<string>> SaveMultipleFilesAsync(ICollection<IFormFile> files, string folderName);
+        Task<string> SaveFileAsync(IFormFile file, string folderName);
+        Task DeleteFile(string filePath);
+        Task DeleteMultipleFiles(List<string> filePaths);
+    }
+}
---
+++ GalleryBackend/Gallery.Business/Concrete/FileService.cs
@@ -0,0 +1,28 @@
+﻿using Gallery.Business.Abstract;
+using Microsoft.AspNetCore.Http;
+
+namespace Gallery.Business.Concrete
+{
+    public class FileService : IFileService
+    {
+        Task IFileService.DeleteFile(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IFileService.DeleteMultipleFiles(List<string> filePaths)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<string> IFileService.SaveFileAsync(IFormFile file, string folderName)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<List<string>> IFileService.SaveMultipleFilesAsync(ICollection<IFormFile> files, string folderName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
